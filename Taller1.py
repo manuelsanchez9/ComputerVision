0d81c88d1d722c455cb739fbca8550a41c22cfe7
Manuel Sanchez <manuelsanchez128354@correo.itm.edu.co>
2018-08-17 09:02:04 -0500

    Taller1.py

---
+++ Taller1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 14:40:53 2018
+
+@author: Manuel Andres Sanchez
+"""
+
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('D:\Computer Vision Repository')
+import my_linealT as trans
+
+Im_g = Image.open('fruit.jpg').convert('L')
+Im_ga = np.array(Im_g)
+Im2 = trans.my_gamma(Im_ga, 25.0)
+plt.gray()
+plt.imshow(np.uint8(Im_ga))
+plt.figure()
+plt.gray()
+Im = Image.fromarray(Im2)
+Im.save('fruit250.jpg')
+plt.imshow(Im2)
